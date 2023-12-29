1625bd70dbf14e7c629be2de38f4a19e2491cf2f
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2023-12-29 12:46:27 +0100

    newGame, added game Object

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -9,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
+  pickCardAnimation = false;
+  game: {} = Game;
+
+  constructor() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    this.pickCardAnimation = true;
+  }
 
 }
