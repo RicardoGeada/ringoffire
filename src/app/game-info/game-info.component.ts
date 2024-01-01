4a5eebfcd258196727a45f52f3e5801f6ea912e6
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-01-01 11:58:46 +0100

    path fixes

--- src/app/game-info/game-info.component.ts
+++ src/app/game-info/game-info.component.ts
@@ -32,8 +32,6 @@ export class GameInfoComponent {
 
   ngOnChanges(): void {
     if (this.card) {
-      console.log('Current card', this.card);
-      console.log('Current number', +this.card.split('_')[1]);
       let cardNumber = +this.card.split('_')[1];
       this.title = this.cardAction[cardNumber - 1].title;
       this.description = this.cardAction[cardNumber - 1].description;
